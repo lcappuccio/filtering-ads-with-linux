44411667e36ded4a7beb69dbd1cbe01a66f81608
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2016-10-05 13:19:56 +0200

    code clean, naming

--- dhcp.php
+++ dhcp.php
@@ -54,10 +54,10 @@
         function getMacAddressVendor($macAddress)
         {
             $url = "http://api.macvendors.com/" . urlencode($macAddress);
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            $response = curl_exec($ch);
+            $curl_resource = curl_init();
+            curl_setopt($curl_resource, CURLOPT_URL, $url);
+            curl_setopt($curl_resource, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($curl_resource);
             if ($response) {
                 return $response;
             } else {
