2eb37fb9acc351fe48958bb7884787bc30d5d670
Leonardo Cappuccio <leo@systemexception.org>
2016-10-07 19:32:21 +0200

    green: find_by_*

--- statsviewer/src/test/java/org/systemexception/adtrap/statsviewer/test/H2ServiceTest.java
+++ statsviewer/src/test/java/org/systemexception/adtrap/statsviewer/test/H2ServiceTest.java
@@ -28,13 +28,13 @@ public class H2ServiceTest {
 	private List<DnsLogLine> dnsLogLineList = new ArrayList<>();
 	private static final Long LOG_ID = 1L;
 	private static final String QUERY_TYPE = "query[A]", QUERY_DOMAIN = "somedomain.org", QUERY_TARGET = "127.0.0.1";
+	private static final Date NOW_DATE = Date.from(Instant.now());
 
 	@Before
 	public void setUp() {
-		Date nowDate = Date.from(Instant.now());
 		dnsLogLine = new DnsLogLine();
 		dnsLogLine.setLogId(LOG_ID);
-		dnsLogLine.setDate(nowDate);
+		dnsLogLine.setDate(NOW_DATE);
 		dnsLogLine.setQueryType(QUERY_TYPE);
 		dnsLogLine.setQueryDomain(QUERY_DOMAIN);
 		dnsLogLine.setQueryTarget(QUERY_TARGET);
@@ -43,15 +43,15 @@ public class H2ServiceTest {
 		dnsLogLineList.add(dnsLogLine);
 		when(dataRepository.save(dnsLogLine)).thenReturn(dnsLogLine);
 		when(dataRepository.findOne(LOG_ID)).thenReturn(dnsLogLine);
-		when(dataRepository.findByDate(nowDate)).thenReturn(dnsLogLineList);
+		when(dataRepository.findByDate(NOW_DATE)).thenReturn(dnsLogLineList);
 		when(dataRepository.findByQueryType(QUERY_TYPE)).thenReturn(dnsLogLineList);
 		when(dataRepository.findByQueryDomain(QUERY_DOMAIN)).thenReturn(dnsLogLineList);
 		when(dataRepository.findByQueryTarget(QUERY_TARGET)).thenReturn(dnsLogLineList);
-		sut = new H2Service(dataRepository);
 	}
 
 	@Test
 	public void should_save_data() {
+		sut = new H2Service(dataRepository);
 		DnsLogLine saved = sut.save(dnsLogLine);
 
 		assertTrue(saved.equals(dnsLogLine));
@@ -60,9 +60,46 @@ public class H2ServiceTest {
 
 	@Test
 	public void should_find_by_id() {
+		sut = new H2Service(dataRepository);
 		DnsLogLine one = sut.findOne(LOG_ID);
 
 		assertTrue(one.equals(dnsLogLine));
 		verify(dataRepository).findOne(LOG_ID);
+	}
+
+	@Test
+	public void should_find_by_date() {
+		sut = new H2Service(dataRepository);
+		List<DnsLogLine> byDate = sut.findByDate(NOW_DATE);
+
+		assertTrue(byDate.equals(dnsLogLineList));
+		verify(dataRepository).findByDate(NOW_DATE);
+	}
+
+	@Test
+	public void should_find_by_query_type() {
+		sut = new H2Service(dataRepository);
+		List<DnsLogLine> byQueryType = sut.findByQueryType(QUERY_TYPE);
+
+		assertTrue(byQueryType.equals(dnsLogLineList));
+		verify(dataRepository).findByQueryType(QUERY_TYPE);
+	}
+
+	@Test
+	public void should_find_by_query_domain() {
+		sut = new H2Service(dataRepository);
+		List<DnsLogLine> byQueryDomain = sut.findByQueryDomain(QUERY_DOMAIN);
+
+		assertTrue(byQueryDomain.equals(dnsLogLineList));
+		verify(dataRepository).findByQueryDomain(QUERY_DOMAIN);
+	}
+
+	@Test
+	public void should_find_by_query_target() {
+		sut = new H2Service(dataRepository);
+		List<DnsLogLine> byQueryTarget = sut.findByQueryTarget(QUERY_TARGET);
+
+		assertTrue(byQueryTarget.equals(dnsLogLineList));
+		verify(dataRepository).findByQueryTarget(QUERY_TARGET);
 	}
 }
