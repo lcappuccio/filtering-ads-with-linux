96fbc8ca1e96ca87e517ccac03b03c424766aa50
Leonardo Cappuccio <leo@systemexception.org>
2016-10-07 17:28:03 +0200

    wip

---
+++ statsviewer/src/main/java/org/systemexception/adtrap/statsviewer/Application.java
@@ -0,0 +1,17 @@
+package org.systemexception.adtrap.statsviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leo
+ * @date 07/10/2016 17:14
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ statsviewer/src/main/java/org/systemexception/adtrap/statsviewer/model/DnsLogLine.java
@@ -0,0 +1,20 @@
+package org.systemexception.adtrap.statsviewer.model;
+
+import java.util.Date;
+
+/**
+ * @author leo
+ * @date 07/10/2016 17:15
+ */
+public class DnsLogLine {
+
+	private long dataId;
+
+	private Date date;
+
+	private String action;
+
+	private String actionDomain;
+
+	private String actionRequester;
+}
