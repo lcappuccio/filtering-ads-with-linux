02b238c964c1de1a503366c92a7c683f60bf1c5a
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2016-11-02 08:46:39 +0100

    wip HttpConnector

--- logtailer/src/main/java/org/systemexception/adtrap/logtailer/services/HttpConnector.java
+++ logtailer/src/main/java/org/systemexception/adtrap/logtailer/services/HttpConnector.java
@@ -3,6 +3,10 @@ package org.systemexception.adtrap.logtailer.services;
 import org.systemexception.logger.api.Logger;
 import org.systemexception.logger.impl.LoggerImpl;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -21,11 +25,31 @@ public class HttpConnector implements Runnable {
 	public void run() {
 		try {
 			while (true) {
-				String take = (String) blockingQueue.take();
-				LOGGER.info("Sent " + take);
+				String logLine = (String) blockingQueue.take();
+				postLine(blockingQueue, logLine);
+				LOGGER.info("Sent " + logLine);
 			}
 		} catch (InterruptedException | ClassCastException e) {
 			LogTailerListener.logInterruptedException(e);
+		}
+	}
+
+	// TODO Add tests
+	private void postLine(final LinkedBlockingQueue queue, final String logLine) throws InterruptedException {
+		try {
+			URL url = new URL("http://localhost:8080/ecommuter/person/add");
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestMethod("POST");
+			urlConnection.setDoOutput(true);
+			urlConnection.setConnectTimeout(1000);
+			urlConnection.setRequestProperty("Content-Type", "application/json");
+			urlConnection.setRequestProperty("Accept", "application/json");
+			OutputStreamWriter osw = new OutputStreamWriter(urlConnection.getOutputStream());
+			osw.write(logLine);
+			osw.flush();
+			osw.close();
+		} catch (IOException ex) {
+			LOGGER.error("Unreachable", ex);
 		}
 	}
 }
