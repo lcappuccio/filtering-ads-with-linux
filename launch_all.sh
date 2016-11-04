5b4fc55a214601ff3d51234b6081c4ed123c5106
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2016-11-04 12:25:06 +0100

    fix bash scripts

--- launch_all.sh
+++ launch_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf *log target/*log nohup.out
-nohup java -jar logtailer-1.0-SNAPSHOT-jar-with-dependencies.jar -f /var/log/dnsmasq.log -s 1000 &
 nohup java -jar logarchiver-1.0-SNAPSHOT.jar &
+nohup java -jar logtailer-1.0-SNAPSHOT-jar-with-dependencies.jar -f /var/log/dnsmasq.log -s 1000 &
--- stop_all.sh
+++ stop_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Grabs and kill a process from the pidlist that has the word myapp
 
-pid=`ps aux | grep logarchiver | awk '{print $2}'`
+pid=`ps aux | grep logarchiver | grep java | awk '{print $2}'`
+kill -9 ${pid}
+
+pid=`ps aux | grep logtailer | grep java | awk '{print $2}'`
 kill -9 ${pid}
