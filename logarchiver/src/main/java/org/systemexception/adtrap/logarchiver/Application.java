dcba16f4479f54468ea429e9f52e360fb2a3ff70
Leonardo Cappuccio <leo@systemexception.org>
2016-11-07 21:52:36 +0100

    unused imports

--- logarchiver/src/main/java/org/systemexception/adtrap/logarchiver/Application.java
+++ logarchiver/src/main/java/org/systemexception/adtrap/logarchiver/Application.java
@@ -1,7 +1,5 @@
 package org.systemexception.adtrap.logarchiver;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
