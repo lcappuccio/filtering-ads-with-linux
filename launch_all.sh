32896372142f3479e33e40a771186fb03a1e8c7b
Leonardo Cappuccio <leo@systemexception.org>
2016-11-09 00:26:53 +0100

    add memory to fix issue with log4j2 log rotation on logtailer

--- launch_all.sh
+++ launch_all.sh
@@ -2,4 +2,4 @@
 
 rm -rf *db *.log* target/*.log* nohup.out
 nohup java -Xmx256m -Xms128m -jar logarchiver-1.0.jar &
-nohup java -Xmx128m -Xms64m -jar logtailer-1.0-jar-with-dependencies.jar -f /var/log/dnsmasq.log -s 1000 &
+nohup java -Xmx256m -Xms128m -jar logtailer-1.0-jar-with-dependencies.jar -f /var/log/dnsmasq.log -s 1000 &
