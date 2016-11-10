4732b940fda979eb9983e786b27698a2b0545e9d
Leonardo Cappuccio <leo@systemexception.org>
2016-11-10 14:16:32 +0100

    catch rest client exception and return default N/A

--- logarchiver/src/main/java/org/systemexception/adtrap/logarchiver/pojo/DhcpFileParser.java
+++ logarchiver/src/main/java/org/systemexception/adtrap/logarchiver/pojo/DhcpFileParser.java
@@ -1,11 +1,13 @@
 package org.systemexception.adtrap.logarchiver.pojo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.systemexception.adtrap.logarchiver.model.DhcpLease;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -14,15 +16,16 @@ import java.util.List;
  */
 public class DhcpFileParser {
 
-	private final static String RECORD_SEPARATOR = " ";
-	private final static int DATE_POSITION = 0;
-	private final static int HOSTNAME_POSITION = 3;
-	private final static int IP_ADDRESS_POSITION = 2;
-	private final static int MAC_ADDRESS_POSITION = 1;
+	private static final Logger LOGGER = LoggerFactory.getLogger(DhcpFileParser.class);
+	private static final String RECORD_SEPARATOR = " ";
+	private static final int DATE_POSITION = 0;
+	private static final int HOSTNAME_POSITION = 3;
+	private static final int IP_ADDRESS_POSITION = 2;
+	private static final int MAC_ADDRESS_POSITION = 1;
 
 	public static List<DhcpLease> parseFileContents(List<String> fileLines) {
 		List<DhcpLease> dhcpLeases = new ArrayList<>();
-		for (String fileLine: fileLines) {
+		for (String fileLine : fileLines) {
 			String[] split = fileLine.split(RECORD_SEPARATOR);
 			DhcpLease dhcpLease = new DhcpLease();
 			dhcpLease.setLeaseExpireDate(Long.parseLong(split[DATE_POSITION]));
@@ -32,14 +35,11 @@ public class DhcpFileParser {
 			dhcpLease.setVendor(getVendor(split[MAC_ADDRESS_POSITION]));
 			dhcpLeases.add(dhcpLease);
 		}
-		Collections.sort(dhcpLeases, new Comparator<DhcpLease>() {
-			@Override
-			public int compare(DhcpLease o1, DhcpLease o2) {
-				if (!o1.getHostname().equals(o2.getHostname())) {
-					return o1.getHostname().compareToIgnoreCase(o2.getHostname());
-				} else {
-					return o1.getIpAddress().compareTo(o2.getIpAddress());
-				}
+		Collections.sort(dhcpLeases, (o1, o2) -> {
+			if (!o1.getHostname().equals(o2.getHostname())) {
+				return o1.getHostname().compareToIgnoreCase(o2.getHostname());
+			} else {
+				return o1.getIpAddress().compareTo(o2.getIpAddress());
 			}
 		});
 		return dhcpLeases;
@@ -47,6 +47,11 @@ public class DhcpFileParser {
 
 	private static String getVendor(String macAddress) {
 		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.getForObject("http://api.macvendors.com/" + macAddress, String.class);
+		try {
+			return restTemplate.getForObject("http://api.macvendors.com/" + macAddress, String.class);
+		} catch (RestClientException e) {
+			LOGGER.error("Error in mac address api");
+			return "N/A";
+		}
 	}
 }
