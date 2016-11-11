3e8755663c90b4ded66be45bb6938a3b9fd83fcb
Leonardo Cappuccio <leo@systemexception.org>
2016-11-11 18:26:20 +0100

    declare functions before using

--- src/main/resources/public/js/systemmonitor.js
+++ src/main/resources/public/js/systemmonitor.js
@@ -3,6 +3,39 @@
 google.charts.load("current", {"packages": ["gauge"]});
 google.charts.setOnLoadCallback(drawChart);
 
+function timeConversion(millisec) {
+	"use strict";
+
+	var seconds = (millisec / 1000).toFixed(1);
+	var minutes = (millisec / (1000 * 60)).toFixed(1);
+	var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+	var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+	if (seconds < 60) {
+		return seconds + " Sec";
+	} else if (minutes < 60) {
+		return minutes + " Min";
+	} else if (hours < 24) {
+		return hours + " Hrs";
+	} else {
+		return days + " Days";
+	}
+}
+
+function formatBytes(bytes, decimals) {
+	"use strict";
+
+	if (bytes === 0) {
+		return "0 Byte";
+	}
+	var k = 1000; // or 1024 for binary
+	var dm = decimals + 1 || 3;
+	var sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+	var i = Math.floor(Math.log(bytes) / Math.log(k));
+
+	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
 function getMetricsJsonData() {
 	"use strict";
 
@@ -88,39 +121,6 @@ function getDatabaseStatus() {
 	var databaseStatus = jsonLines.db.status;
 
 	return ("UP" === databaseStatus);
-}
-
-function timeConversion(millisec) {
-	"use strict";
-
-	var seconds = (millisec / 1000).toFixed(1);
-	var minutes = (millisec / (1000 * 60)).toFixed(1);
-	var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-	var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-	if (seconds < 60) {
-		return seconds + " Sec";
-	} else if (minutes < 60) {
-		return minutes + " Min";
-	} else if (hours < 24) {
-		return hours + " Hrs";
-	} else {
-		return days + " Days";
-	}
-}
-
-function formatBytes(bytes, decimals) {
-	"use strict";
-
-	if (bytes === 0) {
-		return "0 Byte";
-	}
-	var k = 1000; // or 1024 for binary
-	var dm = decimals + 1 || 3;
-	var sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-	var i = Math.floor(Math.log(bytes) / Math.log(k));
-
-	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
 function drawChart() {
