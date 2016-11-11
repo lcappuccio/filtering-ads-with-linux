e403c02a17b5802a73ed0b9eca47685f33b3f30a
Leonardo Cappuccio <leo@systemexception.org>
2016-11-11 01:24:55 +0100

    not used methods

--- src/main/java/org/systemexception/adtrap/model/DnsLogLine.java
+++ src/main/java/org/systemexception/adtrap/model/DnsLogLine.java
@@ -49,29 +49,6 @@ public class DnsLogLine {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		DnsLogLine that = (DnsLogLine) o;
-
-		if (date != that.date) return false;
-		if (queryType != null ? !queryType.equals(that.queryType) : that.queryType != null) return false;
-		if (queryDomain != null ? !queryDomain.equals(that.queryDomain) : that.queryDomain != null) return false;
-		return queryTarget != null ? queryTarget.equals(that.queryTarget) : that.queryTarget == null;
-
-	}
-
-	@Override
-	public int hashCode() {
-		int result = (int) (date ^ (date >>> 32));
-		result = 31 * result + (queryType != null ? queryType.hashCode() : 0);
-		result = 31 * result + (queryDomain != null ? queryDomain.hashCode() : 0);
-		result = 31 * result + (queryTarget != null ? queryTarget.hashCode() : 0);
-		return result;
-	}
-
-	@Override
 	public String toString() {
 		return queryType + LogParser.LOG_LINE_SEPARATOR + queryDomain + LogParser.LOG_LINE_SEPARATOR + queryTarget;
 	}
