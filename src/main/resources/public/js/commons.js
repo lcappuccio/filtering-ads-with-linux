5a3960e885bb987ff72e0a18f746824fa03fd32b
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2016-11-15 09:58:33 +0100

    code style

--- src/main/resources/public/js/commons.js
+++ src/main/resources/public/js/commons.js
@@ -1,32 +1,5 @@
 commons = {};
 
-commons.textResponseToArray = function(responseText, columnName) {
-	"use strict";
-
-	var jsonData = JSON.parse(responseText);
-	var jsonArray = [];
-	jsonArray.push([columnName, "TOTAL"]);
-
-	$.each(jsonData, function (key, value) {
-		var array = [];
-		$.each(value, function (key2, value2) {
-			array.push(value2);
-		});
-		jsonArray.push(array);
-	});
-	return new google.visualization.arrayToDataTable(jsonArray);
-};
-
-commons.getRestResponse = function(restUrl, restResponseType) {
-	"use strict";
-
-	return $.ajax({
-		url: restUrl,
-		dataType: restResponseType,
-		async: false
-	}).responseText;
-};
-
 commons.optionsBarChart = {
 	width: 800,
 	height: 600,
@@ -52,4 +25,31 @@ commons.optionsPieChart = {
 		top: 5
 	},
 	sliceVisibilityThreshold: 0.04
+};
+
+commons.getRestResponse = function(restUrl, restResponseType) {
+	"use strict";
+
+	return $.ajax({
+		url: restUrl,
+		dataType: restResponseType,
+		async: false
+	}).responseText;
+};
+
+commons.textResponseToArray = function(responseText, columnName) {
+	"use strict";
+
+	var jsonData = JSON.parse(responseText);
+	var jsonArray = [];
+	jsonArray.push([columnName, "TOTAL"]);
+
+	$.each(jsonData, function (key, value) {
+		var array = [];
+		$.each(value, function (key2, value2) {
+			array.push(value2);
+		});
+		jsonArray.push(array);
+	});
+	return new google.visualization.arrayToDataTable(jsonArray);
 };
