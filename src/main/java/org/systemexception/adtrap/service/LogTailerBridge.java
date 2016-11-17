740053bd87533cca2dbb649f870f4c94b7b71aa5
Leonardo Cappuccio <leo@systemexception.org>
2016-11-18 00:11:45 +0100

    to public, so codacy is happy

--- src/main/java/org/systemexception/adtrap/service/LogTailerBridge.java
+++ src/main/java/org/systemexception/adtrap/service/LogTailerBridge.java
@@ -33,7 +33,7 @@ public class LogTailerBridge {
 	 * Posts data taken from the queue
 	 */
 	@Scheduled(cron = "* * * * * *")
-	private void postData() throws ParseException, InterruptedException {
+	public void postData() throws ParseException, InterruptedException {
 		int queueSize = logQueue.size();
 		for (int i = 0; i < queueSize; i++) {
 			String queueItem = (String) logQueue.take();
