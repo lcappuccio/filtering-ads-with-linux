3529227fe45b852c197df1ce13f2e01ce912794b
Leonardo Cappuccio <leo@systemexception.org>
2016-11-24 23:30:44 +0100

    ignore case on mac vendor, test non existing dhcp lease file

--- src/test/java/org/systemexception/adtrap/test/DhcpLeasesReaderTest.java
+++ src/test/java/org/systemexception/adtrap/test/DhcpLeasesReaderTest.java
@@ -22,17 +22,21 @@ import static org.junit.Assert.assertEquals;
 public class DhcpLeasesReaderTest {
 
 	private List<DhcpLease> dhcpLeases;
+	private DhcpLeasesReader sut;
+	private File file;
 
 	@Before
 	public void setUp() throws IOException, URISyntaxException {
 		URL systemResource = ClassLoader.getSystemResource("sample.leases");
-		File file = new File(systemResource.toURI());
-		DhcpLeasesReader sut = new DhcpLeasesReader(file.getAbsolutePath());
-		dhcpLeases = sut.getDhcpLeases();
+		file = new File(systemResource.toURI());
 	}
 
 	@Test
 	public void should_read_file() {
+
+		sut = new DhcpLeasesReader(file.getAbsolutePath());
+
+		dhcpLeases = sut.getDhcpLeases();
 
 		assertNotNull(dhcpLeases);
 		assertEquals(3, dhcpLeases.size());
@@ -41,6 +45,9 @@ public class DhcpLeasesReaderTest {
 	@Test
 	public void should_have_valid_values() {
 
+		sut = new DhcpLeasesReader(file.getAbsolutePath());
+
+		dhcpLeases = sut.getDhcpLeases();
 		DhcpLease dhcpLease = dhcpLeases.get(0);
 
 		assertTrue(dhcpLease.getLeaseExpireDate().contains("07/11/2016") &&
@@ -48,6 +55,18 @@ public class DhcpLeasesReaderTest {
 		assertEquals("00:50:56:31:51:28", dhcpLease.getMacAddress());
 		assertEquals("192.168.0.102", dhcpLease.getIpAddress());
 		assertEquals("host1", dhcpLease.getHostname());
-		assertTrue("VMware, Inc.".equals(dhcpLease.getVendor()) || "N/A".equals(dhcpLease.getVendor()));
+		assertTrue("VMWARE, INC.".equalsIgnoreCase(dhcpLease.getVendor()) ||
+				"N/A".equals(dhcpLease.getVendor()));
+	}
+
+	@Test
+	public void should_log_nonexisting_file() throws URISyntaxException, IOException {
+
+		File file = new File("nonexisting.file");
+		sut = new DhcpLeasesReader(file.getAbsolutePath());
+
+		dhcpLeases = sut.getDhcpLeases();
+
+		assertEquals(0, dhcpLeases.size());
 	}
 }
