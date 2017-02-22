be2a42515253b021e224afa9e034929a1473d3ad
Leo <leo@systemexception.org>
2017-02-22 17:01:15 +0100

    ignored domains added to database

--- src/main/resources/public/js/admin.js
+++ src/main/resources/public/js/admin.js
@@ -1,5 +1,15 @@
 /*eslint no-undef: "error"*/
-/* global $, commons */
+/* global $, commons, jQuery */
+
+function addIgnoredDomain() {
+    var findByNameString = document.getElementById("addDomainTextField").value;
+    $.ajax({
+        data: jQuery.param({domain: findByNameString}),
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        url: commons.context + "addignoreddomain",
+        type: "POST"
+    });
+}
 
 $(document).ready(function () {
     commons.hideLoadingGears();
