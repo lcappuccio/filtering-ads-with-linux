798eadd190d32236d9e8fd72615dfd215ff2d92c
Leo <leo@systemexception.org>
2017-02-22 18:29:34 +0100

    add images to security config

--- src/main/java/org/systemexception/adtrap/SecurityConfig.java
+++ src/main/java/org/systemexception/adtrap/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 				.antMatchers("/css**").permitAll()
 				.antMatchers("/health").permitAll()
+				.antMatchers("/images/**").permitAll()
 				.antMatchers("/metrics").permitAll()
 				.antMatchers("/**").hasRole(USER_ROLE)
 				.and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/clientlist", true)
