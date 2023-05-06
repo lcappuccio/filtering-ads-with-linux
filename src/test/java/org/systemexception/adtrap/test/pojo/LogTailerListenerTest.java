0fc38bd625fa3f9d12e5bec1678c385a2a1da433
Leonardo <lcappuccio@some-random-email.org>
2023-05-06 18:30:25 +0200

    stabilize test attempt #1

--- src/test/java/org/systemexception/adtrap/test/pojo/LogTailerListenerTest.java
+++ src/test/java/org/systemexception/adtrap/test/pojo/LogTailerListenerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -32,7 +31,6 @@ import static org.springframework.test.util.AssertionErrors.assertTrue;
 @SpringBootTest(classes = {Application.class})
 @WebAppConfiguration
 @TestPropertySource(locations = "classpath:application-test.properties")
-@DirtiesContext
 public class LogTailerListenerTest {
 
 	private static File testLogFile, testLogFileRotate;
